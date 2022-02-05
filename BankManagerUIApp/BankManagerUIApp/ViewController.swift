f9e086ac8e88e14c3fabc60a0ef18f817f11df56
oksunwoo <oksunwo@naver.com>
2022-02-05 18:30:06 +0900

    refactor: Storyboard 제거

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -10,6 +10,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
