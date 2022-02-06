dd0f03e5d7fde81514fe341cf9acb414c70102a5
oksunwoo <oksunwo@naver.com>
2022-02-06 19:12:47 +0900

    feat: 고객번호 할당 구현

--- BankManagerUIApp/BankManagerUIApp/Controller/MainViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/Controller/MainViewController.swift
@@ -20,6 +20,8 @@ class MainViewController: UIViewController {
     let timeTextLabel = UILabel()
     let waitingLabel = UILabel()
     let workingLabel = UILabel()
+    var bankManager = BankManager(bank: Bank(numberOfClerksForLoans: 1, numberOfClerksForDeposits: 2))
+    var clientCount = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,11 +61,11 @@ class MainViewController: UIViewController {
         
         addCustomerButton.setTitle("고객 10명 추가", for: .normal)
         addCustomerButton.setTitleColor(.systemBlue, for: .normal)
-        addCustomerButton.addTarget(self, action: #selector(addCustomer), for: .touchUpInside)
+        addCustomerButton.addTarget(self, action: #selector(touchUpButton), for: .touchUpInside)
         
         resetButton.setTitle("초기화", for: .normal)
         resetButton.setTitleColor(.systemRed, for: .normal)
-        resetButton.addTarget(self, action: #selector(resetBank), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(touchUpButton), for: .touchUpInside)
     
         buttonStackView.addArrangedSubview(addCustomerButton)
         buttonStackView.addArrangedSubview(resetButton)
@@ -149,80 +151,31 @@ class MainViewController: UIViewController {
         processStackView.addArrangedSubview(workingStackView)
     }
     
-    @objc func addCustomer() {
-        print("10명 추가")
+    @objc func touchUpButton(sender: UIButton) {
+        switch sender {
+        case addCustomerButton:
+            let clients = generateClients()
+            bankManager.openBank(for: clients)
+        case resetButton:
+            clientCount = 1
+        default:
+            print("")
+        }
     }
-    
-    @objc func resetBank() {
-        print("초기화")
+  
+    func generateClients() -> [Client] {
+        let clientNumber = 10
+        var clients: [Client] = []
+        
+        (clientCount...clientNumber + clientCount - 1).forEach { number in
+            let orderTicket = Ticket(number: number)
+            let randomTaskIndex = Int.random(in: 0..<Task.allCases.count)
+            let randomTask = Task.allCases[randomTaskIndex]
+            let client = Client(orderTicket: orderTicket, task: randomTask)
+            clients.append(client)
+        }
+        
+        clientCount += clientNumber
+        return clients
     }
-    
 }
-
-
-//
-//enum MenuOption: String {
-//   case openBank = "1"
-//   case exit = "2"
-//}
-//
-////MARK: - 메서드
-//func showMenu() {
-//   let menu = """
-//       1 : 은행개점
-//       2 : 종료
-//       입력 :
-//       """
-//   print(menu, terminator: " ")
-//}
-//
-//func getUserInput() throws -> MenuOption {
-//   guard let input = readLine(),
-//         let menuOption = MenuOption(rawValue: input) else {
-//       throw BankManagerError.invalidUserInput
-//   }
-//
-//   return menuOption
-//}
-//
-//func generateClients() -> [Client] {
-//   let clientNumber = Int.random(in: 10...30)
-//   var clients: [Client] = []
-//
-//   (1...clientNumber).forEach { number in
-//       let orderTicket = Ticket(number: number)
-//       let randomTaskIndex = Int.random(in: 0..<Task.allCases.count)
-//       let randomTask = Task.allCases[randomTaskIndex]
-//       let client = Client(orderTicket: orderTicket, task: randomTask)
-//       clients.append(client)
-//   }
-//
-//   return clients
-//}
-//
-//func runProgram() {
-//   while true {
-//       do {
-//           showMenu()
-//           let userInput = try getUserInput()
-//
-//           switch userInput {
-//           case .openBank:
-//               let clients = generateClients()
-//               bankManager.openBank(for: clients)
-//           case .exit:
-//               return
-//           }
-//       } catch BankManagerError.invalidUserInput {
-//           print(BankManagerError.invalidUserInput.description)
-//           print("다시 입력해주십시오.\n")
-//       } catch {
-//           print(error)
-//       }
-//   }
-//}
-//
-//let bank = Bank(numberOfClerksForLoans: 1, numberOfClerksForDeposits: 2)
-//var bankManager = BankManager(bank: bank)
-//
-//runProgram()
